417e3f8f71cfdf0a68749b76a41ec1a1e268b7a7
samsonnjosh <samsonmwanginjosh@gmail.com>
2021-01-21 16:25:24 +0300

    this is my first commit

---
+++ python-operators/operators.py
@@ -0,0 +1,35 @@
+#Arithmetic operators
+num1 =100
+num2 =20
+print("num1+num2=",num1+num2) #additional operator
+print("num1-num2=",num1-num2) #substraction operator
+print("num1*num2=",num1*num2) #multiplication operator
+print("num1/num2=",num1/num2) #divisional operator
+print("2^3=",2**3)            #exponential operator
+print("22//7=",22//7)          #float division
+
+#Assignment operators
+num3 = num1+num2 # the right handside operations are computed and assigned to num3
+print("num3=",num3)
+
+#comparison operators
+print("Is num2<num3 ?",num3>num2)
+print("Is num2 =num3 ?",num2==num3)
+print("Isnum1 !=num2 ?", num1!=num2)
+
+#logical operators
+x=True 
+y=False
+print("x and y", x and y)
+print("x or y", x or y)
+print("not of x",not x)
+print("not of y",not y)
+
+#Bitwise operators
+num4 =6 #110 binary value
+num5 =2 #010
+print("Bitwise and x", num4&num5)
+print("Bitwise or =",num4 | num5)
+print("Biwise xor =", num4^num5)
+
+
